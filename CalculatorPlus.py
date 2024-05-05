e1f2bb61164e19c9a62996c3725a3e6b646a8f69
Saiganesh Angadi <saiganesh7989@gmail.com>
2024-05-05 13:29:17 +0530

    Code Onboarding
    Copy pasted the code for further

---
+++ CalculatorPlus.py
@@ -0,0 +1,38 @@
+import math
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+# TODO: Implement the following function to calculate the square root of a number.
+
+# def square_root(self, x):
+
+# return math.sqrt(x)
+
+# You need to uncomment the above function and complete its implementation to add the square root feature.
+
+if __name__ == "__main__":
+
+    calculator = Calculator()
+    num1 = 16
+    num2 = 4
+    print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
+    print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
+    print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+    print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+# TODO: Uncomment and test the square root feature.
+
+# num3 = 25
+
+# print(f"The square root of {num3} = {calculator.square_root(num3)}")
